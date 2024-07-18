e7c49611a025f1d6900cd4ef8603a6e4e8eff8a9
qwezsd <avwds@naver.com>
2024-07-18 19:43:15 +0900

    powerpoint

--- src/wrap/WrapComponent.jsx
+++ src/wrap/WrapComponent.jsx
@@ -3,12 +3,14 @@ import WindowComponent from './WindowComponent';
 import HeaderComponent from './HeaderComponent';
 import MenuComponent from './MenuComponent';
 import FooterComponent from './FooterComponent';
+import MainComponent from './MainComponent';
 
 export default function WrapComponent (){
     return (
         <div id='wrap'>
             <WindowComponent />
             <HeaderComponent />
+            <MainComponent />
             <MenuComponent />
             <FooterComponent />
         </div>
