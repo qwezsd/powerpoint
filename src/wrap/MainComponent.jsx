beb830aa463b4afc6213cc38aaa913258c93782a
qwezsd <avwds@naver.com>
2024-07-22 22:38:26 +0900

    powerpoint

--- src/wrap/MainComponent.jsx
+++ src/wrap/MainComponent.jsx
@@ -6,10 +6,19 @@ export default function MainComponent () {
         <div id='main'>
             {/* 이 부분을 메인슬라이드로 */}
             {/* 왼쪽에 목록 있는 건 반응형처럼 만들어서 슬라이드 사이즈 줄어들 때마다 내용들 줄일 수 있게 하기 */}
-            <div className="container">
-                <div className="content">
-                    <div className="slide">
-                        <div className="box">
+            <div className="left-container">
+                <div className="left-content">
+                    <div className="left-slide">
+                        <div className="left-box">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="right-container">
+                <div className="right-content">
+                    <div className="right-slide">
+                        <div className="right-box">
                             
                         </div>
                     </div>
