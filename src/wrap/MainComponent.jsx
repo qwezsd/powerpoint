2a79d7e3bbb06720bce562138e1d4093eea337b5
qwezsd <avwds@naver.com>
2024-07-30 23:57:42 +0900

    powerpoint

--- src/wrap/MainComponent.jsx
+++ src/wrap/MainComponent.jsx
@@ -4,7 +4,6 @@ import './sass/main.scss'
 export default function MainComponent () {
     return (
         <div id='main'>
-            {/* 왼쪽에 목록 있는 건 반응형처럼 만들어서 슬라이드 사이즈 줄어들 때마다 내용들 줄일 수 있게 하기 */}
             <div className="left-container">
                 <div className="left-content">
                     <div className="left-slide">
